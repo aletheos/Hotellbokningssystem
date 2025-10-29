bbc39649c9ae1a842e7126e8b3853c4a4600d96b
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-10-29 15:17:40 +0100

    create customer model

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,4 +1,54 @@
 package model;
 
 public class Customer {
+    private int customer_id;
+    private String name;
+    private String email;
+    private String city;
+
+
+    public Customer(String name, String email, String city) {
+        this.name = name;
+        this.email = email;
+        this.city = city;
+    }
+
+    public Customer(int customer_id, String name, String email, String city) {
+        this.customer_id = customer_id;
+        this.name = name;
+        this.email = email;
+        this.city = city;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
