b39936d6cd60f3d01f9e583c8812da9fe3d6de7a
Marco <laasonen.marco@yahoo.com>
2025-10-29 15:37:49 +0100

    add db connection

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,16 @@
 package app;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            Connection conn = Database.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Done");
     }
 }
