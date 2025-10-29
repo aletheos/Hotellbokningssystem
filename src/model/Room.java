82707746ddc4771ebf47e93f98439b5f6d8e27b8
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-10-29 15:27:45 +0100

    create Room class

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,4 +1,61 @@
 package model;
 
+import java.math.BigDecimal;
+
 public class Room {
+    private int room_id;
+    private RoomType type;
+    private BigDecimal price;
+
+    public enum RoomType {
+        DOUBLE_ROOM,
+        SINGLE_ROOM,
+        FAMILY_ROOM
+    }
+
+    public Room(RoomType type, BigDecimal price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public Room(int room_id, RoomType type, BigDecimal price) {
+        this.room_id = room_id;
+        this.type = type;
+        this.price = price;
+    }
+
+    public int getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(int room_id) {
+        this.room_id = room_id;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "room_id=" + room_id +
+                ", type=" + type +
+                ", price=" + price +
+                '}';
+    }
 }
+
+
