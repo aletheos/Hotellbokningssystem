cbc0c9556f030a8c05d15c05087e2b8f92792298
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-10-30 13:47:25 +0100

    start with RoomDao

--- src/dao/RoomDAO.java
+++ src/dao/RoomDAO.java
@@ -1,4 +1,15 @@
 package dao;
 
+import model.Room;
+
+import java.util.List;
+
 public interface RoomDAO {
+
+    String sql = """
+            CREATE TABLE IF NOT EXISTS rooms(
+            room_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            type ENUM('DOUBLE_ROOM', 'SINGLE_ROOM', 'FAMILY_ROOM') NOT NULL,
+            price DECIMAL(10,2))
+            """;
 }
