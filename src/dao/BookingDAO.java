96fc81f14a2b286e9dc0759e80844ee2437e0cc7
Marco <laasonen.marco@yahoo.com>
2025-10-30 15:01:16 +0100

    add bookingDAO

--- src/dao/BookingDAO.java
+++ src/dao/BookingDAO.java
@@ -1,4 +1,28 @@
 package dao;
 
+import model.Booking;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface BookingDAO {
+
+    /*
+     * CRUD:
+     * CREATE    -> addBooking()
+     * READ      -> getAllBookings()
+     * READ      -> getBooking()
+     * UPDATE    -> updateBooking()
+     * DELETE    -> deleteBooking()
+     * */
+
+    public void addBooking(LocalDate start_date, LocalDate end_date, int customer_id, int room_id);
+
+    public List<Booking> getAllBookings();
+
+    public Booking getBooking( int booking_id);
+
+    public void updateBooking( int booking_id, LocalDate start_date, LocalDate end_date, int customer_id, int room_id);
+
+    public void deleteBooking(int booking_id);
 }
