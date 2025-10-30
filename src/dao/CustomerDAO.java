3ab9f3da13ec4fb39027e5cb50deb12732e626d5
aletheos <aletheos@outlookcom>
2025-10-30 11:18:11 +0100

    Add method signatures to interface and implementation > Begin work on addCustomer()

--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -1,4 +1,12 @@
 package dao;
 
+import model.Customer;
+import java.util.List;
+
 public interface CustomerDAO {
+    public void addCustomer(Customer customer);
+    public List<Customer> getAllCustomers();
+    public Customer findCustomerByEmail(String email);
+    public void updateCustomerCity(Customer customer, String city);
+    public void deleteCustomer(Customer customer);
 }
--- src/dao/CustomerDAOImpl.java
+++ src/dao/CustomerDAOImpl.java
@@ -1,4 +1,44 @@
 package dao;
 
+import db.Database;
+import model.Customer;
+import java.sql.*;
+import java.util.List;
+
 public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public void addCustomer(Customer customer) {
+        String sql = "INSERT INTO customers(name, email, city) VALUE(?, ?, ?)";
+        try(
+            Connection connection = Database.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+            statement.setString(1, customer.getName());
+            statement.setString(2, customer.getEmail());
+            statement.setString(3, customer.getCity());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public Customer findCustomerByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerCity(Customer customer, String city) {
+
+    }
+
+    @Override
+    public void deleteCustomer(Customer customer) {
+
+    }
 }
