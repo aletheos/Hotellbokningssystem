be87f19c763dd726d740390b96f6cc11c22db4a0
aletheos <aletheos@outlookcom>
2025-10-31 10:19:48 +0100

    Delete test-code from main

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,22 +10,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        // TEST-CODE! DELETE FROM HERE
-        CustomerDAO dao = new CustomerDAOImpl();
-        String email = "Boll@kråkfötter.nu";
-        Customer customer = new Customer(100, "Grodan Boll", email, "Övik");
-        dao.addCustomer(customer);
-        customer = dao.findCustomerByEmail(email);
-        dao.updateCustomerCity(customer.getCustomerId(), "Örnsköldsvik");
-        System.out.println();
-        System.out.println(dao.getAllCustomers());
-        dao.deleteCustomer(customer.getCustomerId());
-        customer = dao.findCustomerByEmail(email);
-        System.out.println(customer);
-        System.out.println();
-        System.out.println(dao.getAllCustomers());
-        // TEST-CODE! DELETE TO HERE
-
         try  (Connection conn = Database.getConnection()) {
             Hotel.getInstance().run();
         } catch (SQLException e) {
